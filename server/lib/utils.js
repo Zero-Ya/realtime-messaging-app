46cbcce1ebf3ca9a3d6d7813a26bb7464d591ee5
Zero-Ya <ketyanuk111@gmail.com>
2025-04-14 09:44:27 +0700

    More server-side debugging

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -1,9 +1,13 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = (userId, res) => {
+    console.log(userId, res)
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
         expiresIn: "7d",
     });
+    console.log(process.env.JWT_SECRET)
+
+    console.log("First token:",token)
 
     res.cookie("token", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
@@ -11,6 +15,8 @@ const generateToken = (userId, res) => {
         sameSite: true,
         secure: process.env.NODE_ENV !== "development",
     })
+
+    console.log("Second token:", token)
 
     return token;
 }
